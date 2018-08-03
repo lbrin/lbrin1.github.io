0cf793f6ae63682e37bc7f6da237a3f6456e4f81
lbrin <1959605653@qq.com>
2018-08-03 16:12:18 +0800

    测试

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function demo(){
+//	for(var i = 0;i <9;i++){
+////		document.write("富强");
+//		window.alert("和谐");
+//	}
+	while(true){
+		window.alert("文明和谐公正法治富强民主")
+//		document.write("文明和谐公正法治富强民主");
+	}
+	
+}
