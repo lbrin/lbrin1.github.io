8658d052026267dbd081cd57e692cc32ed1088fa
lbrin <1959605653@qq.com>
2018-08-10 14:47:24 +0800

    demo

--- js/index.js
+++ js/index.js
@@ -1,11 +1,26 @@
-function demo(){
-//	for(var i = 0;i <9;i++){
-////		document.write("富强");
-//		window.alert("和谐");
-//	}
-	while(true){
-		window.alert("文明和谐公正法治富强民主")
-//		document.write("文明和谐公正法治富强民主");
-	}
-	
+window.onload = function () {
+    // 1. 求出屏幕的尺寸
+    var screenW = document.documentElement.clientWidth;
+    var screenH = document.documentElement.clientHeight;
+
+// 2. 动态创建星星
+for(var i=0; i<150; i++){
+    // 2.1 创建星星
+    var span = document.createElement('span');
+    document.body.appendChild(span);
+
+    // 2.2 随机的坐标
+    var x = parseInt(Math.random() * screenW);
+    var y = parseInt(Math.random() * screenH);
+    span.style.left = x + 'px';
+    span.style.top = y + 'px';
+
+    // 2.3 随机缩放
+    var scale = Math.random() * 1.5;
+    span.style.transform = 'scale('+ scale + ', ' + scale + ')';
+
+    // 2.4 频率
+    var rate = Math.random() * 1.5;
+    span.style.animationDelay = rate + 's';
+    }
 }
